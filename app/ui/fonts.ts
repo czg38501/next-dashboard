1fbd83f086a825d0da5a2e9a8f3ee898e94e5cd5
czg <17610050801@163.com>
2023-12-17 20:55:42 +0800

    231217修改

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import {Inter, Lunasima} from "next/font/google";
+
+export const inter = Inter(
+    {subsets: ["latin"]}
+)
+
+export const lusitana = Lunasima({
+    weight: ['400', '700'],
+    subsets: ["latin"]
+})
